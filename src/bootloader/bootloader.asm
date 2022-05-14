cb2b455402a2f7d087656b6002a4daf94b058b2e
shyam314159 <79404413+shyam314159@users.noreply.github.com>
2022-05-14 10:58:57 +0530

    Update bootloader.asm

--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -10,23 +10,23 @@ bits 16
 jmp short start
 nop
 
-bdb_oem:			        db 'MSWIN4.1'
+bdb_oem:			db 'MSWIN4.1'
 bdb_bytes_per_sector:		dw 512
 bdb_sectors_per_cluster:	db 1
 bdb_reserved_sectors:		dw 1
-bdb_fat_count:			    db 2
+bdb_fat_count:		 	db 2
 bdb_dir_entries_count:		dw 0E0h
-bdb_total_sectors:		    dw 2880
+bdb_total_sectors:		dw 2880
 bdb_media_descriptor_type:	db 0F0h
 bdb_sectors_per_fat:		dw 9
 bdb_sectors_per_track:		dw 18
-bdb_heads:			        dw 2
-bdb_hidden_sectors:		    dd 0
+bdb_heads:		        dw 2
+bdb_hidden_sectors:		dd 0
 bdb_large_sector_count:		dd 0
 
 ; extended boot record
 ebr_drive_number:		db 0
-				        db 0
+			        db 0
 ebr_signature:			db 0
 ebr_volume_id:			db 0, 0, 0, 0
 ebr_volume_label:		db '           '
